0f0ad975f496bbefa1d07cc0fd898ad438d4d314
Michiel <git@fastfox.nl>
2014-03-08 21:43:05 +0100

    task4

--- task4.py
+++ task4.py
@@ -1,11 +1,20 @@
-from numpy import zeros, random
+from numpy import zeros, random, dot, array
 
 def sketch(M, k):
-	sketches = zeros(shape=(5, 5))
-	print k
-	print sketches
+	rd = random.randn(k, len(M))
+	sketches = zeros((k, len(M[0])))
+	for i in range(k):
+		for j in range(len(M[0])):
+			v = dot(rd[i], M[:, j])
+			if v > 0:
+				sketches[i][j] = 1
+			elif v < 0:
+				sketches[i][j] = -1
+			else:
+				if random.random() >= 0.5:
+					sketches[i][j] = 1
+				else:
+					sketches[i][j] = -1
+	return sketches
 
-	rd = random.rand(k) # Random direction
-	print rd
-
-sketch([1.0, 2.0, 3.0], 2)
+sketch(array([[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]), 2)
