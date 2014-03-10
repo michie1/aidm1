32ab88843b4a838f46fc027c561f853d32c6b95e
Michiel <git@fastfox.nl>
2014-03-10 19:13:55 +0100

    task3 fix

--- task3.py
+++ task3.py
@@ -1,10 +1,16 @@
-from scipy import spatial
-from numpy import dot
+from numpy import dot, pi, arccos, allclose
 from math import sqrt, acos, pi
 
-
 def cossim(s1, s2):
-	return  1 - acos(dot(s1, s2) / (sqrt(dot(s1, s1)) * sqrt(dot(s2, s2)))) / pi
+	#return  1 - acos(dot(s1, s2) / (sqrt(dot(s1, s1)) * sqrt(dot(s2, s2)))) / pi
+	hoi = dot(s1, s2) / dot(linalg.norm(s1),linalg.norm(s2))
+	if allclose(hoi, 1):
+		ac = 0
+	else:
+		ac = arccos(hoi)
+	return 1 - ac
+
+
 	
-print cossim([1, 2, 3], [3, 4, 5])
-print cossim([1, 2, 3], [1, 2, 3])
+#print cossim([1, 2, 3], [3, 4, 5])
+#print cossim([1, 2, 3], [1, 2, 3])
